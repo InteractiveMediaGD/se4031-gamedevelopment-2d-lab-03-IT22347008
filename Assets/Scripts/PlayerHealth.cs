825adc86736a5c708cf949d5106c0ba79defc253
Pahan Amarasekara <pahanamarasekara24@gmail.com>
2026-02-08 22:13:25 +0530

    Initial Commit

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public GameObject fatalPanel;
+
+    public TMP_Text healthText;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        fatalPanel.SetActive(false);
+        UpdateUI();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth < 0) currentHealth = 0;
+
+        UpdateUI();
+
+        if (currentHealth < 50)
+        {
+            healthText.color = Color.yellow;
+        }
+
+        if (currentHealth <= 10)
+        {
+            healthText.color = Color.red;
+            FatalMode();
+        }
+
+        if (currentHealth == 0)
+        {
+            Time.timeScale = 0f;
+        }
+
+    }
+
+    public void ResetHealth()
+    {
+        currentHealth = maxHealth;
+        Time.timeScale = 1f;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        healthText.text = "Health: " + currentHealth;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    private void FatalMode()
+    {
+        fatalPanel.SetActive(true);
+    }
+}
